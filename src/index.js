c17bf7b07148bdf7a96421dbe7cb0c3d51554ab3
Oleh <oleh.tsarevych@gmail.com>
2024-02-21 23:45:05 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import App from './components/App';
-import { GlobalStyle } from './GlobalStyle';
+import App from './components/App.js';
+import { GlobalStyle } from './GlobalStyle.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
